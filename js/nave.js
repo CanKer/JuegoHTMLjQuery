e868f2055a0dcd243d23e37f2a2f2d026ab467df
Jorge Arturo César Martínez <cdialike@gmail.com>
2015-08-21 00:49:27 -0500

    CoordenadasBalas
    Se muestran en el style de jugador, hay que checar eso para que se
    muestren en el label de coordenadas. Falta configurar bien la bala,
    tiene muchos errores y falta código.

--- js/nave.js
+++ js/nave.js
@@ -3,6 +3,14 @@ var $xp = 0, $yp =0;
 $(document).ready(function()  {
 
   $('#jugador').click(function() {
+    $('#contenedor').click(function() {
+      $('#bala').animate({
+        opacity: 1,
+        top: "-=5000",
+        height: "toggle"
+
+      }, 2500);
+    });
    alert("al darme click debería disparar pero aún no programo eso xD");
   });
 
@@ -15,8 +23,14 @@ $(document).ready(function()  {
   // change 12 to alter damping higher is slower
   $xp += (($mouseX - $xp)/12);
 //  $yp += (($mouseY - $yp)/12);
+
   $("#jugador").css({left:$xp +'px', top:$yp +'500'});
+
+  var jugador = $("#jugador").css({left:$xp +'px', top:$yp +'500'});
+
+
   }, 30);
+    $("#coordenadas").html($(jugador).val());
 
 
 });
