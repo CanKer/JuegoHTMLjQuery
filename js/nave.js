c58f694c4eb7c4d62c551967cec46fcb332d32e4
Jorge Arturo César Martínez <cdialike@gmail.com>
2015-08-23 22:30:29 -0500

    Listo
    Falta Header y Footer

--- js/nave.js
+++ js/nave.js
@@ -1,36 +1,56 @@
-var $mouseX = 0, $mouseY = 0;
-var $xp = 0, $yp =0;
-$(document).ready(function()  {
+var $mouseX = 0,
+  $mouseY = 0;
+var $xp = 0,
+  $yp = 0;
+$(document).ready(function() {
 
-  $('#jugador').click(function() {
-    $('#contenedor').click(function() {
-      $('#bala').animate({
-        opacity: 1,
-        top: "-=5000",
-        height: "toggle"
+  /* $(document).mousemove(function() {
+     var p = $("#coordenadas").html($(jugador).val());
+     $('#bala').position(p);
+   });
+*/
 
-      }, 2500);
+  // var p = $("#coordenadas").html($(jugador).val());
+  // $('#bala').position(p);
+
+  $(document).mousemove(function(e) {
+    $mouseX = e.pageX;
+    //     $mouseY = e.pageY;
+  });
+
+  var $loop = setInterval(function() {
+    // change 12 to alter damping higher is slower
+    $xp += (($mouseX - $xp) / 12);
+    //  $yp += (($mouseY - $yp)/12);
+
+    $("#jugador").css({
+      left: $xp + 'px',
+      top: $yp + '500'
     });
-   alert("al darme click debería disparar pero aún no programo eso xD");
-  });
 
-  $(document).mousemove(function(e){
-     $mouseX = e.pageX;
-//     $mouseY = e.pageY;
-  });
-
-  var $loop = setInterval(function(){
-  // change 12 to alter damping higher is slower
-  $xp += (($mouseX - $xp)/12);
-//  $yp += (($mouseY - $yp)/12);
-
-  $("#jugador").css({left:$xp +'px', top:$yp +'500'});
-
-  var jugador = $("#jugador").css({left:$xp +'px', top:$yp +'500'});
-
-
+    var jugador = $("#jugador").css({
+      left: $xp + 'px',
+      top: $yp + '500'
+    });
   }, 30);
-    $("#coordenadas").html($(jugador).val());
 
+  $("#coordenadas").html($(jugador).val());
+
+  $('body').click(function() {
+    $('#bala').animate({
+      opacity: 1,
+      top: "-=581",
+      height: "toggle"
+    }, 750);
+    if ($('#bala').css('top') == '-1px') {
+      //    x=$("#jugador").offset().left;
+      //    y=$("#jugador").offset().top;
+      $("#bala").css({
+        left: 500,
+        top: 580
+      });
+
+    }
+  });
 
 });
