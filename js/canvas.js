f36a4f7b2056fbf67a614802d5a1712529ed9a16
Jorge Arturo César Martínez <cdialike@gmail.com>
2015-09-02 20:52:56 -0500

    Disparos  (Arturo-Cristina)
    Se agregó: Disparos, movimiento nave. fondo canvas. Falta cambiar color
    bala, enemigos disparen y mueran.

--- js/canvas.js
+++ js/canvas.js
@@ -6,6 +6,9 @@ var alien3 = document.getElementById('alien3');
 var alien4 = document.getElementById('alien4');
 var alien5 = document.getElementById('alien5');
 
+
+//$('#ctx').css('background-color', 'rgba(158, 167, 184, 0.2)');
+
 ctx.font = '30px Arial';
 ctx.fillStyle = 'red';
 ctx.globalAlpha = 1;
@@ -44,70 +47,70 @@ enemigos('E4', 50, 40, 20, 40, 'o');
 enemigos('E5', 40, 50, 10, 50, 'o'); */
 
 //Bloque izquierdo
-enemigos('E01', 140, 150, -10, 0, alien1);
-enemigos('E02', 190, 150, -10, 0, alien2);
-enemigos('E03', 240, 150, -10, 0, alien3);
-enemigos('E04', 290, 150, -10, 0, alien4);
-enemigos('E05', 340, 150, -10, 0, alien5);
-enemigos('E06', 140, 200, 10, 0, alien1);
-enemigos('E07', 190, 200, 10, 0, alien2);
-enemigos('E08', 240, 200, 10, 0, alien3);
-enemigos('E09', 290, 200, 10, 0, alien4);
-enemigos('E10', 340, 200, 10, 0, alien5);
-enemigos('E11', 140, 250, -10, 0, alien1);
-enemigos('E12', 190, 250, -10, 0, alien2);
-enemigos('E13', 240, 250, -10, 0, alien3);
-enemigos('E14', 290, 250, -10, 0, alien4);
-enemigos('E15', 340, 250, -10, 0, alien5);
-enemigos('E16', 140, 300, 10, 0, alien1);
-enemigos('E17', 190, 300, 10, 0, alien2);
-enemigos('E18', 240, 300, 10, 0, alien3);
-enemigos('E19', 290, 300, 10, 0, alien4);
-enemigos('E20', 340, 300, 10, 0, alien5);
+enemigos('E01', 140, 150, -1, 0, alien1);
+enemigos('E02', 190, 150, -1, 0, alien2);
+enemigos('E03', 240, 150, -1, 0, alien3);
+enemigos('E04', 290, 150, -1, 0, alien4);
+enemigos('E05', 340, 150, -1, 0, alien5);
+enemigos('E06', 140, 200, 1, 0, alien1);
+enemigos('E07', 190, 200, 1, 0, alien2);
+enemigos('E08', 240, 200, 1, 0, alien3);
+enemigos('E09', 290, 200, 1, 0, alien4);
+enemigos('E10', 340, 200, 1, 0, alien5);
+enemigos('E11', 140, 250, -1, 0, alien1);
+enemigos('E12', 190, 250, -1, 0, alien2);
+enemigos('E13', 240, 250, -1, 0, alien3);
+enemigos('E14', 290, 250, -1, 0, alien4);
+enemigos('E15', 340, 250, -1, 0, alien5);
+enemigos('E16', 140, 300, 1, 0, alien1);
+enemigos('E17', 190, 300, 1, 0, alien2);
+enemigos('E18', 240, 300, 1, 0, alien3);
+enemigos('E19', 290, 300, 1, 0, alien4);
+enemigos('E20', 340, 300, 1, 0, alien5);
 
 //Bloque medio
-enemigos('E21', 440, 150, -10, 0, alien1);
-enemigos('E22', 490, 150, -10, 0, alien2);
-enemigos('E23', 540, 150, -10, 0, alien3);
-enemigos('E24', 590, 150, -10, 0, alien4);
-enemigos('E25', 640, 150, -10, 0, alien5);
-enemigos('E26', 440, 200, 10, 0, alien1);
-enemigos('E27', 490, 200, 10, 0, alien2);
-enemigos('E28', 540, 200, 10, 0, alien3);
-enemigos('E29', 590, 200, 10, 0, alien4);
-enemigos('E30', 640, 200, 10, 0, alien5);
-enemigos('E31', 440, 250, -10, 0, alien1);
-enemigos('E32', 490, 250, -10, 0, alien2);
-enemigos('E33', 540, 250, -10, 0, alien3);
-enemigos('E34', 590, 250, -10, 0, alien4);
-enemigos('E35', 640, 250, -10, 0, alien5);
-enemigos('E36', 440, 300, 10, 0, alien1);
-enemigos('E37', 490, 300, 10, 0, alien2);
-enemigos('E38', 540, 300, 10, 0, alien3);
-enemigos('E39', 590, 300, 10, 0, alien4);
-enemigos('E40', 640, 300, 10, 0, alien5);
+enemigos('E21', 440, 150, -1, 0, alien1);
+enemigos('E22', 490, 150, -1, 0, alien2);
+enemigos('E23', 540, 150, -1, 0, alien3);
+enemigos('E24', 590, 150, -1, 0, alien4);
+enemigos('E25', 640, 150, -1, 0, alien5);
+enemigos('E26', 440, 200, 1, 0, alien1);
+enemigos('E27', 490, 200, 1, 0, alien2);
+enemigos('E28', 540, 200, 1, 0, alien3);
+enemigos('E29', 590, 200, 1, 0, alien4);
+enemigos('E30', 640, 200, 1, 0, alien5);
+enemigos('E31', 440, 250, -1, 0, alien1);
+enemigos('E32', 490, 250, -1, 0, alien2);
+enemigos('E33', 540, 250, -1, 0, alien3);
+enemigos('E34', 590, 250, -1, 0, alien4);
+enemigos('E35', 640, 250, -1, 0, alien5);
+enemigos('E36', 440, 300, 1, 0, alien1);
+enemigos('E37', 490, 300, 1, 0, alien2);
+enemigos('E38', 540, 300, 1, 0, alien3);
+enemigos('E39', 590, 300, 1, 0, alien4);
+enemigos('E40', 640, 300, 1, 0, alien5);
 
 //Bloque derecho
-enemigos('E41', 740, 150, -10, 0, alien1);
-enemigos('E42', 790, 150, -10, 0, alien2);
-enemigos('E43', 840, 150, -10, 0, alien3);
-enemigos('E44', 890, 150, -10, 0, alien4);
-enemigos('E45', 940, 150, -10, 0, alien5);
-enemigos('E46', 740, 200, 10, 0, alien1);
-enemigos('E47', 790, 200, 10, 0, alien2);
-enemigos('E48', 840, 200, 10, 0, alien3);
-enemigos('E49', 890, 200, 10, 0, alien4);
-enemigos('E50', 940, 200, 10, 0, alien5);
-enemigos('E51', 740, 250, -10, 0, alien1);
-enemigos('E52', 790, 250, -10, 0, alien2);
-enemigos('E53', 840, 250, -10, 0, alien3);
-enemigos('E54', 890, 250, -10, 0, alien4);
-enemigos('E55', 940, 250, -10, 0, alien5);
-enemigos('E56', 740, 300, 10, 0, alien1);
-enemigos('E57', 790, 300, 10, 0, alien2);
-enemigos('E58', 840, 300, 10, 0, alien3);
-enemigos('E59', 890, 300, 10, 0, alien4);
-enemigos('E60', 940, 300, 10, 0, alien5);
+enemigos('E41', 740, 150, -1, 0, alien1);
+enemigos('E42', 790, 150, -1, 0, alien2);
+enemigos('E43', 840, 150, -1, 0, alien3);
+enemigos('E44', 890, 150, -1, 0, alien4);
+enemigos('E45', 940, 150, -1, 0, alien5);
+enemigos('E46', 740, 200, 1, 0, alien1);
+enemigos('E47', 790, 200, 1, 0, alien2);
+enemigos('E48', 840, 200, 1, 0, alien3);
+enemigos('E49', 890, 200, 1, 0, alien4);
+enemigos('E50', 940, 200, 1, 0, alien5);
+enemigos('E51', 740, 250, -1, 0, alien1);
+enemigos('E52', 790, 250, -1, 0, alien2);
+enemigos('E53', 840, 250, -1, 0, alien3);
+enemigos('E54', 890, 250, -1, 0, alien4);
+enemigos('E55', 940, 250, -1, 0, alien5);
+enemigos('E56', 740, 300, 1, 0, alien1);
+enemigos('E57', 790, 300, 1, 0, alien2);
+enemigos('E58', 840, 300, 1, 0, alien3);
+enemigos('E59', 890, 300, 1, 0, alien4);
+enemigos('E60', 940, 300, 1, 0, alien5);
 
 //Jugador
 jugadores = function(id, x, y, velX, velY, vidas, fig) {
@@ -126,23 +129,67 @@ var listaJugadores = {};
 
 jugadores('J1', 0, 660, 100, 0, 5, naveJ);
 
+balas = function(id){
+var bala =	{
+	x: listaJugadores['J1'].x + 55,
+	y: listaJugadores['J1'].y,
+	velX: 0,
+	velY: 10,
+	width: 10,
+	height: 10,
+	angulo: 100,
+	id: id,
+};
+ listaBalas[id] = bala;
+}
+
+var listaBalas = {};
+	var id = 0;
+document.onclick = function()	{
+
+	balas(id);
+	id++;
+	console.log(balas());
+}
+
+
+
 var ALTURA = 720;
 var ANCHO = 1080;
 
 document.onmousemove = function(mouse)  {
   var mouseX = mouse.clientX;
   var mouseY = mouse.clientY;
-  
+
+		if(mouseX < listaJugadores['J1'].width/2)	{
+			mouseX = listaJugadores['J1'].width/2;
+		}	if (mouseX > ANCHO-listaJugadores['J1'].width/2) {
+			mouseX = ANCHO - listaJugadores['J1'].width/2;
+		}	if (mouseY < listaJugadores['J1'].height/2) {
+			mouseY = listaJugadores['J1'].height/2;
+		}	if (mouseY > ALTURA - listaJugadores['J1'].height/2) {
+			mouseY = ALTURA - listaJugadores['J1'].height/2;
+		}
+
   listaJugadores['J1'].x = mouseX;
   dibujarEntidadP(listaJugadores['J1']);
-  console.log(listaJugadores['J1'].x = mouseX);						
+//  console.log(listaJugadores['J1'].x = mouseX);
   //listaJugadores['J1'].y = mouseY;
 }
 
 movimiento = function(entidad) {
   dibujarEntidadA(entidad);
-  dibujarMovimientos(entidad);
+	dibujarMovimientos(entidad);
   dibujarEntidadP(listaJugadores['J1']);
+
+}
+movimientoB = function(entidad)	{
+	dibujarEntidadB(entidad);
+	dibujarMovimientosB(entidad);
+}
+
+dibujarMovimientosB = function(entidad) {
+  entidad.y -= entidad.velY;
 }
 
 dibujarMovimientos = function(entidad) {
@@ -155,43 +202,36 @@ dibujarMovimientos = function(entidad) {
   if (entidad.y <= 0 || entidad.y >= ALTURA) {
       entidad.velY = -entidad.velY;
   }
-  //console.log(x);
 }
 
 dibujarEntidadP = function(entidad) {
- /*naveJ.onload = function(entidad){
-  ctx.drawImage(entidad, destX, destY);
-};*/
  ctx.drawImage(entidad.fig, entidad.x, 600, 120, 100);
- //ctx.fillText(entidad.fig, entidad.x, entidad.y);
 }
 dibujarEntidadA = function(entidad) {
-  //ctx.drawImage(listaJugadores['J1'].fig, entidad.x, entidad.y);
-  //ctx.fillText(entidad.fig, entidad.x, entidad.y);
   ctx.drawImage(entidad.fig, entidad.x, entidad.y, 40, 40);
 }
+dibujarEntidadB = function(entidad) {
+  ctx.fillRect(entidad.x, entidad.y, entidad.width, entidad.height);
+}
+
 
 update = function() {
-	//ctx.fillStyle = '#FFF';
-	//ctx.fillRect(0,0,1080,720);
-  ctx.clearRect(0,0, ANCHO, ALTURA);
 
+  ctx.clearRect(0,0, ANCHO, ALTURA);
+	for(var id in listaBalas)	{
+		movimientoB(listaBalas[id]);
+	}
   for (var id in listaEnemigos)  {
     movimiento(listaEnemigos[id]);
     var colision = hacerColision(listaJugadores['J1'],listaEnemigos[id]);
     if (colision) {
       listaJugadores['J1'].vidas = listaJugadores['J1'].vidas - 1;
-      console.log('Perdiste');
+    //  console.log('Perdiste');
       var tiempoTotal = tiempoInicio+=1;
     }
   }
-  //ctx.drawImage(listaJugadores['J1'].fig, 0,0);
-  //var grafico = ctx.drawImage(listaJugadores['J1'].fig, 0,0);
+
   dibujarEntidadP(listaJugadores['J1'].fig);
   ctx.fillText("Vidas: "+ listaJugadores['J1'].vidas + "           Tiempo: " + tiempoTotal, 50, 50);
 }
-  setInterval(update, 250);
-
-//  ctx.fillText('hello', 50, 50);
-//  ctx.fillRect(50,50, 100, 100);
-//  ctx.clearRect(75, 75, 50, 50);
+  setInterval(update, 10);
